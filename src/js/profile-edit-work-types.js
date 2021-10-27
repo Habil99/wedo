706c51ecb8c27feff1f49f0023fdf0866e12ee36
habil99 <abiyevhabil3@gmail.com>
2021-10-27 13:51:11 +0400

    profil edit work types js completed

---
+++ src/js/profile-edit-work-types.js
@@ -0,0 +1,20 @@
+$(function () {
+  function toggleSections(type) {
+    if (type === "show-edit") {
+      $(".work-types .btn-edit").fadeOut();
+      $(".work-types-cover").fadeOut();
+      $(".work-types-edit").removeClass("hidden");
+      $(".work-types-edit").fadeIn();
+    } else {
+      $(".work-types .btn-edit").fadeIn();
+      $(".work-types-cover").fadeIn();
+      $(".work-types-edit").addClass("hidden");
+      $(".work-types-edit").fadeOut();
+    }
+  }
+
+  $(".work-types .btn-edit").on("click", () => toggleSections("show-edit"));
+
+  $(".work-types .btn-save").on("click", toggleSections)
+  $(".work-types .btn-reject").on("click", toggleSections)
+});
